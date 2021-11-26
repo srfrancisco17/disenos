02c6b6450886b192cf3bc9da80f18feca53fc452
Francisco Ortega <srfrancisco17@gmail.com>
2021-11-26 16:13:18 -0500

    #132

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -14,7 +14,16 @@ class BasicDesignScreen extends StatelessWidget {
           //Image
           Image(image: AssetImage('assets/landscape.jpg')),
           //Title widget
-          Title()
+          Title(),
+
+          //Button Section
+          ButtonSection(),
+
+          //Descripcion
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat')
+          ),
         ] 
       ),
    );
@@ -41,6 +50,77 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, text: 'Call'),
+          CustomButton(icon: Icons.room_outlined, text: 'Route'),
+          CustomButton(icon: Icons.share, text: 'Share'),
+        ],
+      ),
+    );
+
+    /*
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Icon(Icons.call, color: Colors.blue),
+              Text('CALL', style: TextStyle(color: Colors.blue)),
+            ],
+          ),
+          Expanded(child: Container()),
+          Column(
+            children: [
+              Icon(Icons.map, color: Colors.blue),
+              Text('ROUTE', style: TextStyle(color: Colors.blue)),
+            ],
+          ),
+          Expanded(child: Container()),
+          Column(
+            children: [
+              Icon(Icons.share, color: Colors.blue),
+              Text('SHARE', style: TextStyle(color:Colors.blue))
+            ],
+          ),
+        ],
+      ),
+    );
+    */
+  }
+}
+
+class CustomButton extends StatelessWidget {
+
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key, 
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue, size: 30),
+        Text(this.text, style: TextStyle(color: Colors.blue)),
+      ],
     );
   }
 }
