b30ef5f6db2481de7bfd784975460011d734174a
Francisco Ortega <srfrancisco17@gmail.com>
2021-12-02 21:35:11 -0500

    138

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:disenos/screens/basic_design.dart';
+import 'package:disenos/screens/home_screen.dart';
 import 'package:disenos/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
 
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      initialRoute: 'scroll_design',
+      initialRoute: 'home_design',
       routes: {
         'basic_design': (_) => BasicDesignScreen(),
         'scroll_design': (_) => ScrollScreen(),
+        'home_design': (_) => HomeScreen(),
       },
     );
   }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:disenos/widgets/background.dart';
+import 'package:disenos/widgets/custom_bottom_navigation.dart';
+import 'package:disenos/widgets/page_title.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          //Background
+          Background(),
+
+          _HomeBody(),
+        ],
+      ),
+      bottomNavigationBar: CustomBottomNavigation(),
+   );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          PageTitle()
+        ],
+      ),
+    );
+  }
+}
